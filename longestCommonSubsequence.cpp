40f70d05488061e221327e22e8fdb62e0921335c
MilindKulkarni12 <milindrk12@gmail.com>
2020-08-27 17:51:56 +0530

    Longest Common Subsequence Problem Bottom-up

--- longestCommonSubsequence.cpp
+++ longestCommonSubsequence.cpp
@@ -27,6 +27,23 @@ int lcsTopDown(string x, string y, int n, int m)
         return t[n][m] = max(lcsTopDown(x, y, n, m - 1), lcsTopDown(x, y, n - 1, m));
 } //lcsTopDown
 
+int lcsBottomUp(string x, string y, int n, int m)
+{
+    for (int i = 0; i < n + 1; ++i)
+        for (int j = 0; j < m + 1; ++j)
+            if (i == 0 || j == 0)
+                t[i][j] = 0;
+
+    for (int i = 1; i < n + 1; ++i)
+        for (int j = 1; j < m + 1; ++j)
+            if (x[i - 1] == y[j - 1])
+                t[i][j] = 1 + t[i - 1][j - 1];
+            else
+                t[i][j] = max(t[i][j - 1], t[i - 1][j]);
+
+    return t[n][m];
+} //lcsBottomUp
+
 int main()
 {
     string x, y;
@@ -34,7 +51,8 @@ int main()
     cin >> x >> y;
     cout << "Longest Common Subsequence length : "
          // << lcs(x, y, x.size(), y.size());
-         << lcsTopDown(x, y, x.size(), y.size());
+         //  << lcsTopDown(x, y, x.size(), y.size());
+         << lcsBottomUp(x, y, x.size(), y.size());
 
     return 0;
 } //main
