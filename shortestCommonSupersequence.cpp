db62712cf302187ed9e2845bb648b0a8ed3fbd30
MilindKulkarni12 <milindrk12@gmail.com>
2020-08-28 15:03:05 +0530

    Shortest Common Supersequence(Length) Problem

---
+++ shortestCommonSupersequence.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t[1001][1001];
+int shortestCommonSupersequence(string x, string y, int n, int m)
+{
+    for (int i = 0; i < n + 1; ++i)
+        for (int j = 0; j < m + 1; ++j)
+            if (i == 0 || j == 0)
+                t[i][j] = 0;
+    for (int i = 1; i < n + 1; ++i)
+        for (int j = 1; j < m + 1; ++j)
+            if (x[i - 1] == y[j - 1])
+                t[i][j] = 1 + t[i - 1][j - 1];
+            else
+                t[i][j] = max(t[i][j - 1], t[i - 1][j]);
+    return n + m - t[n][m];
+} //shortestCommonSupersequence
+
+int main()
+{
+    string x, y;
+    int n, m;
+
+    cin >> x >> y;
+    n = x.length();
+    m = y.length();
+
+    cout << "Shortest Common Subsequence Length : " << shortestCommonSupersequence(x, y, n, m) << endl;
+
+    return 0;
+} //main
