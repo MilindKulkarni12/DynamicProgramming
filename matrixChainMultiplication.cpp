a03f209c53aa9e7d1311188e86ed30f90455adcc
MilindKulkarni12 <milindrk12@gmail.com>
2020-09-01 02:02:01 +0530

    Matrix Chain Multiplication Problem (added top-down)

--- matrixChainMultiplication.cpp
+++ matrixChainMultiplication.cpp
@@ -16,6 +16,25 @@ int mcm(vector<int> &arr, int i, int j)
     return mn;
 } //mcm
 
+int t[1001][1001];
+int mcmTopDown(vector<int> &arr, int i, int j)
+{
+    if (i >= j)
+        return 0;
+
+    if (t[i][j] != -1)
+        return t[i][j];
+
+    int mn = INT_MAX;
+    int temp;
+    for (int k = i; k < j; k++)
+    {
+        temp = mcm(arr, i, k) + mcm(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+        mn = min(temp, mn);
+    } //for
+    return t[i][j] = mn;
+} //mcmTopDown
+
 int main()
 {
     int n;
@@ -23,6 +42,9 @@ int main()
     vector<int> arr(n);
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    cout << "Matrix Chain Multiplication Value : " << mcm(arr, 1, n - 1) << endl;
+    memset(t, -1, sizeof(t));
+    cout << "Matrix Chain Multiplication Value : "
+         // << mcm(arr, 1, n - 1) << endl;
+         << mcmTopDown(arr, 1, n - 1) << endl;
     return 0;
 }
