48145dd6a29d7aa9fc9cb8143bfffbe39e98544c
MilindKulkarni12 <milindrk12@gmail.com>
2020-09-02 00:45:26 +0530

    Minimum cuts for making Palindrome (Memoized)

--- palindromePartitioning.cpp
+++ palindromePartitioning.cpp
@@ -26,11 +26,35 @@ int palindromePartitioning(string s, int i, int j)
     return mn;
 } //palindromePartitioning
 
+int t[1001][1001];
+int palindromePartitioningTopDown(string s, int i, int j)
+{
+    if (i >= j)
+        return 0;
+    if (isPalindrome(s, i, j))
+        return 0;
+
+    if (t[i][j] != -1)
+        return t[i][j];
+
+    int mn = INT_MAX;
+    int temp;
+    for (int k = i; k < j; k++)
+    {
+        temp = palindromePartitioningTopDown(s, i, k) + palindromePartitioningTopDown(s, k + 1, j) + 1;
+        mn = min(mn, temp);
+    } //for
+    return t[i][j] = mn;
+} //palindromePartitioningTopDown
+
 int main()
 {
     string s;
     cin >> s;
+    memset(t, -1, sizeof(t));
     cout << "Minimum Partitions for making Palindrome : "
-         << palindromePartitioning(s, 0, s.length() - 1) << endl;
+         //  << palindromePartitioning(s, 0, s.length() - 1) << endl;
+         << palindromePartitioningTopDown(s, 0, s.length() - 1) << endl;
+
     return 0;
 } //main
